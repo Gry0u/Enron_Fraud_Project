f2b6b724110490c6d6dfb6deb4ef4669256da7b8
gauthier <gauthriou@protonmail.com>
2018-05-18 21:52:22 +0200

    Create classifier and evaluate it

--- evaluation/evaluate_poi_identifier.py
+++ evaluation/evaluate_poi_identifier.py
@@ -13,6 +13,9 @@
 
 import pickle
 import sys
+from sklearn.cross_validation import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import confusion_matrix, precision_score, recall_score
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
 
@@ -27,5 +30,18 @@ labels, features = targetFeatureSplit(data)
 
 
 ### your code goes here 
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size=0.3, random_state=42)
+clf = DecisionTreeClassifier()
+clf.fit(features_train, labels_train)
+print clf.score(features_test, labels_test)
+print 'number POI: ', len([l for l in labels_test if l==1])
+print 'number of people in test set: ', len(labels_test)
+print 'number of true positives: ',confusion_matrix(labels_test, clf.predict(features_test))[1][1]
+print 'precision: ', precision_score(labels_test, clf.predict(features_test))
+print 'recall: ', recall_score(labels_test, clf.predict(features_test))
 
+predictions = [0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1]
+true_labels = [0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 0]
 
+print precision_score(true_labels, predictions)
+print recall_score(true_labels, predictions)
